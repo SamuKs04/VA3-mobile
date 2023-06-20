dcd17878d5803ca5672c752943bb43bc1aa9ae5d
Rian Sousa <riansousadf@gmail.com>
2023-06-20 03:31:11 -0300

    add nova pagina de menu de almoco, e faixa com frase

--- mobile_burguers/lib/main.dart
+++ mobile_burguers/lib/main.dart
@@ -58,6 +58,8 @@ class Home extends StatelessWidget {
 }
 
 class AlmocoScreen extends StatelessWidget {
+  const AlmocoScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,20 +67,50 @@ class AlmocoScreen extends StatelessWidget {
         centerTitle: true,
         title: const Text('Cardápio de Almoço', textAlign: TextAlign.center),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 200,
-              width: double.infinity,
-              child: Image.asset(
-                'lib/img/almoco-menu.jpg',
-                fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: Image.asset(
+                  'lib/img/almoco-menu.jpg',
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-          ],
-        ),
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                      height: 100,
+                      color: Colors.black.withOpacity(0.5),
+                      child: const Center(
+                          child: Text(
+                              'Um bom dia depende de um bom almoço! \n Bem-vindo(a)!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold)))))
+            ],
+          ),
+        ],
       ),
     );
   }
+}
+
+class item {
+  String nome;
+  String descricao;
+  double preco;
+  String? url;
+
+  item(
+      {required this.nome,
+      required this.descricao,
+      required this.preco,
+      this.url});
 }
