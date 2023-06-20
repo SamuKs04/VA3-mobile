29923aea2e7f013360b5baf92acd89d34aa98bda
aoshDevv <vitoxdantas@gmail.com>
2023-06-20 02:00:44 -0300

    Adicionando a rota home

--- mobile_burguers/lib/main.dart
+++ mobile_burguers/lib/main.dart
@@ -12,6 +12,9 @@ class APP extends StatelessWidget{
         primarySwatch: Colors.yellow,
       ),
       initialRoute: '/',
+      routes: {
+        '/':(context) => const Home(),
+      },
     );
   }
 }
@@ -25,8 +28,16 @@ class Home extends StatelessWidget{
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 242, 204, 12),
-        title: const Text('Mobile Burgues', textAlign: TextAlign.center,)
-
+        title: const Text('Mobile Burgues', textAlign: TextAlign.center, style: TextStyle(fontSize: 24),),
+        titleTextStyle: const TextStyle(color: Colors.white),
+      ),
+      body: const Center( 
+        child: Column( 
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Bem Vindes(a) \n '),        
+          ],
+        ),
       ),
     );
   }
