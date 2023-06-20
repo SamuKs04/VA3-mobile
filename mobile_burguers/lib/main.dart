563d8248e8d3c7987920f2cc7df6699a19f68c24
Rian Sousa <riansousadf@gmail.com>
2023-06-20 03:46:05 -0300

    merge manual

--- mobile_burguers/lib/main.dart
+++ mobile_burguers/lib/main.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() { runApp(const APP());}
+void main() {
+  runApp(const APP());
+}
 
-class APP extends StatelessWidget{
-  const APP ({super.key});
+class APP extends StatelessWidget {
+  const APP({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
       initialRoute: '/',
       routes: {
-        '/':(context) => const Home(),
+        '/': (context) => const Home(),
       },
     );
   }
 }
 
-class Home extends StatelessWidget{
-  const Home ({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 242, 204, 12),
@@ -35,7 +37,7 @@ class Home extends StatelessWidget{
         ),
         titleTextStyle: const TextStyle(color: Colors.white),
       ),
-       body: Column(
+      body: Column(
         children: [
           SizedBox(
             height: 200,
@@ -49,7 +51,12 @@ class Home extends StatelessWidget{
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('\n\n Bem Vindo(a) \n Nosso Cardápio está de cara nova! \n Clique no botão abaixo para acessar', style: TextStyle(color: Colors.orangeAccent, fontSize: 22,)),
+                Text(
+                    '\n\n Bem Vindo(a) \n Nosso Cardápio está de cara nova! \n Clique no botão abaixo para acessar',
+                    style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 22,
+                    )),
               ],
             ),
           ),
@@ -58,8 +65,61 @@ class Home extends StatelessWidget{
     );
   }
 }
-      
 
+class AlmocoScreen extends StatelessWidget {
+  const AlmocoScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Cardápio de Almoço', textAlign: TextAlign.center),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: Image.asset(
+                  'lib/img/almoco-menu.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                      height: 100,
+                      color: Colors.black.withOpacity(0.5),
+                      child: const Center(
+                          child: Text(
+                              'Um bom dia depende de um bom almoço! \n Bem-vindo(a)!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold)))))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class item {
+  String nome;
+  String descricao;
+  double preco;
+  String? url;
 
+  item(
+      {required this.nome,
+      required this.descricao,
+      required this.preco,
+      this.url});
+}
