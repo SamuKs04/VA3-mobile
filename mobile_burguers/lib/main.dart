b8be07a5d9b4323ed20a0e8bf48b577a7824a078
Rian Sousa <riansousadf@gmail.com>
2023-06-20 02:55:29 -0300

    primeiro commit dv_rian

--- mobile_burguers/lib/main.dart
+++ mobile_burguers/lib/main.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() { runApp(const APP());}
+void main() {
+  runApp(const APP());
+}
 
-class APP extends StatelessWidget{
-  const APP ({super.key});
+class APP extends StatelessWidget {
+  const APP({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
       initialRoute: '/',
       routes: {
-        '/':(context) => const Home(),
+        '/': (context) => const Home(),
+        '/almoco': (context) => AlmocoScreen()
       },
     );
   }
 }
 
-class Home extends StatelessWidget{
-  const Home ({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,14 +31,25 @@ class Home extends StatelessWidget{
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 242, 204, 12),
-        title: const Text('Mobile Burgues', textAlign: TextAlign.center, style: TextStyle(fontSize: 24),),
+        title: const Text(
+          'Mobile Burgues',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24),
+        ),
         titleTextStyle: const TextStyle(color: Colors.white),
       ),
-      body: const Center( 
-        child: Column( 
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text('Bem Vindes(a) \n '),        
+            const Text('Bem Vindes(a) \n '),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Ver almoços'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/almoco');
+              },
+            )
           ],
         ),
       ),
@@ -43,6 +57,28 @@ class Home extends StatelessWidget{
   }
 }
 
-
-
-
+class AlmocoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Cardápio de Almoço', textAlign: TextAlign.center),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: Image.asset(
+                'lib/img/almoco-menu.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
